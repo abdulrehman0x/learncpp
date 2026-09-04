3a05c8d94c9d7305fe9ec0396109845afb621b9d
Abdul Rehman <abdulrehmanimran945@gmail.com>
2026-09-04 22:58:42 +0500

    basic age calculator

---
+++ day2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int year;
+    int age;
+    std::cout <<"Enter your birth year: "<< std::flush;
+    std::cin >> year;
+    age = (2026 - year);
+    std::cout <<"Your age is:"<< age;
+
+
+
+}
